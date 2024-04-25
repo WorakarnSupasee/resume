d9bd4e417b1052674b3bb5e08a97a490cc31ca06
WorakarnSupasee <168050502+WorakarnSupasee@users.noreply.github.com>
2024-04-25 06:23:01 +0000

    Deploying to gh-pages from @ WorakarnSupasee/resume@7c202d51adfa44814dd67f8300217dc9b249a257 🚀

--- _nuxt/C9QxuxTE.js
+++
@@ -1,1 +0,0 @@
-import{N as i}from"./BC9X6jgi.js";const s=i("/unicity/list.JPG"),o=i("/unicity/Unicity Notify Manament.JPG"),c=i("/unicity/preview.JPG"),n=i("/unicity/homepage.JPG");export{s as _,o as a,c as b,n as c};
