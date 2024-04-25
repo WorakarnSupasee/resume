4385e9a3c3b9bc146791eb7eed1f7f30ae104a46
Worakarn Supasee <worakarn.supa@gmail.com>
2024-04-25 13:19:57 +0700

    [update] svg loder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import svgLoader from "vite-svg-loader"
+
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ['@nuxtjs/color-mode', '@nuxtjs/tailwindcss',]
-  // modules: ['@nuxtjs/color-mode']
+  modules: ['@nuxtjs/color-mode', '@nuxtjs/tailwindcss',],
+  vite: {
+    plugins: [svgLoader()]
+  }
 })
