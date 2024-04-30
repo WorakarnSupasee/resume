34c99c264592d35fefe18d936047a903495fe17d
Worakarn Supasee <worakarn.supa@gmail.com>
2024-04-30 14:16:18 +0700

    update nuxt-img

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,11 @@ import svgLoader from "vite-svg-loader"
 
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ['@nuxtjs/color-mode', '@nuxtjs/tailwindcss',],
+  modules: ['@nuxtjs/color-mode', '@nuxtjs/tailwindcss', "@nuxt/image"],
   vite: {
     plugins: [svgLoader()]
-  }
+  },
+  image: {
+    provider: "ipx"
+}
 })
